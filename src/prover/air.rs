5878f81cc680604e312c2ade483415017561abc6
Kaushik <kausriniv@gmail.com>
2022-12-01 09:44:13 +0530

    adding benchmarking

--- src/prover/air.rs
+++ src/prover/air.rs
@@ -1,28 +1,8 @@
 use winterfell::{
     math::{fields::f128::BaseElement, FieldElement},
-    Air, AirContext, Assertion, ByteWriter, EvaluationFrame, ProofOptions, Serializable, TraceInfo,
+    Air, AirContext, Assertion, EvaluationFrame, ProofOptions, TraceInfo,
     TransitionConstraintDegree,
 };
-
-// Public inputs for our computation will consist of the starting value and the end result.
-// pub struct PublicInputs {
-//     start: BaseElement,
-//     result: BaseElement,
-// }
-
-// // We need to describe how public inputs can be converted to bytes.
-// impl Serializable for PublicInputs {
-//     fn write_into<W: ByteWriter>(&self, target: &mut W) {
-//         target.write(self.start);
-//         target.write(self.result);
-//     }
-// }
-
-// impl PublicInputs {
-//     pub fn new(start: BaseElement, result: BaseElement) -> Self {
-//         Self { start, result }
-//     }
-// }
 
 // For a specific instance of our computation, we'll keep track of the public inputs and
 // the computation's context which we'll build in the constructor. The context is used
